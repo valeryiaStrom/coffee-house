c1af5670562b52de4ea3e444bf11809d76d4e7c2
Valeryia Strom <valeryia_strom@epam.com>
2024-02-10 09:45:38 +0300

    add data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
+const sliderArrowLeft = document.querySelector(".slider__arrow_left");
+const sliderArrowRight = document.querySelector(".slider__arrow_right");
+const slidesWrapper = document.querySelector(".slider__slides");
+const slidesContainer = document.querySelector('.slides');
+const slides = document.querySelectorAll(".slide");
+const SLIDE_ACTIVE_CN = "slide_active";
+
 window.onload = function () {
   console.log("Hello!");
 
-  const sliderArrowRight = document.querySelector(".slider__arrow");
-  const slides = document.querySelector(".slider__slides");
-
-  sliderArrowRight.addEventListener("click", (e) => {
-    console.log(e);
-    slides.style.transform = "translateX(-0%)";
-    e.target.classList.add("active");
-  });
+  // addSliderHandler();
 };
+
+// const addSliderHandler = () => {
+//   sliderArrowRight.addEventListener("click", (e) => {
+//     console.log(e);
+//     console.log("right arrow was clicked");
+//     slides.forEach((slide) => {
+//       if (slide.classList.contains(SLIDE_ACTIVE)) {
+//         slide.classList.remove(SLIDE_ACTIVE);
+//         // slide.style.transform = "translateX(-100%)";
+//         slidesContainer.style.transform = "translateX(-100%)";
+
+//       }
+//     });
+//   });
+// };
