d395abd07342bc2dae129e5f602bf9644356a467
Valeryia Strom <valeryia_strom@epam.com>
2024-02-15 10:44:33 +0300

    add slider functionality

--- src/index.js
+++ src/index.js
@@ -5,74 +5,83 @@ import { MenuContainer } from "./js/menu-container";
 import { MenuItem } from "./js/menu-item";
 import { MenuItemModal } from "./js/menu-item-modal";
 import { ModalService } from "./js/modal-service";
+import { Slider } from "./js/slider";
 
 const menuItemService = new MenuItemService(products);
 const menuItemsContainer = new MenuContainer();
 const tabs = new MenuTabs();
 const modalService = new ModalService(products);
+const slider = new Slider();
 
 window.onload = function () {
   console.log("Hello");
-  // render menu items by default selected category
-  const defaultSelectCategory = tabs.getSelectedTabCategory();
+  const isMenuPage = window.location.href.includes("menu");
+  if (!isMenuPage) {
+    console.log("home page: " + window.location.href);
+    slider.bindEvents();
+  } else {
+    console.log("menu page");
+    // render menu items by default selected category
+    const defaultSelectCategory = tabs.getSelectedTabCategory();
 
-  // filter products data by category
-  const productsData = menuItemService.filterDataByCategory(
-    defaultSelectCategory
-  );
+    // filter products data by category
+    const productsData = menuItemService.filterDataByCategory(
+      defaultSelectCategory
+    );
 
-  // create menu items from filtered data
-  const menuItems = productsData.map((productData) => {
-    const menuItem = new MenuItem(productData);
-    return menuItem.createMenuItemElement();
-  });
+    // create menu items from filtered data
+    const menuItems = productsData.map((productData) => {
+      const menuItem = new MenuItem(productData);
+      return menuItem.createMenuItemElement();
+    });
 
-  // render menu items
-  menuItemsContainer.renderMenuItems(menuItems);
+    // render menu items
+    menuItemsContainer.renderMenuItems(menuItems);
 
-  // handle menu tabs click
-  tabs.tabsContainer.addEventListener("click", (e) => {
-    if (e.target.closest(".tab")) {
-      const clickedTab = e.target.closest(".tab");
-      const clickedTabId = clickedTab.getAttribute("data-id");
+    // handle menu tabs click
+    tabs.tabsContainer.addEventListener("click", (e) => {
+      if (e.target.closest(".tab")) {
+        const clickedTab = e.target.closest(".tab");
+        const clickedTabId = clickedTab.getAttribute("data-id");
 
-      // unselect all tabs
-      tabs.unselectAllTabs();
+        // unselect all tabs
+        tabs.unselectAllTabs();
 
-      // select clicked tab
-      tabs.selectTab(clickedTab);
+        // select clicked tab
+        tabs.selectTab(clickedTab);
 
-      // filter products data by category
-      const productsData = menuItemService.filterDataByCategory(clickedTabId);
+        // filter products data by category
+        const productsData = menuItemService.filterDataByCategory(clickedTabId);
 
-      // create menu items from filtered data
-      const menuItems = productsData.map((productData) => {
-        const menuItem = new MenuItem(productData);
-        return menuItem.createMenuItemElement();
-      });
+        // create menu items from filtered data
+        const menuItems = productsData.map((productData) => {
+          const menuItem = new MenuItem(productData);
+          return menuItem.createMenuItemElement();
+        });
 
-      // clear displayed menu items
-      menuItemsContainer.clear();
+        // clear displayed menu items
+        menuItemsContainer.clear();
 
-      // render menu items
-      menuItemsContainer.renderMenuItems(menuItems);
-    }
-  });
+        // render menu items
+        menuItemsContainer.renderMenuItems(menuItems);
+      }
+    });
 
-  // handle menu item click
-  menuItemsContainer.self.addEventListener("click", (e) => {
-    if (e.target.closest(".menu__item")) {
-      console.log("menu item was clicked");
-      const clickedMenuItem = e.target.closest(".menu__item");
-      const clickedMenuItemId = clickedMenuItem.getAttribute("data-id");
-      console.log(clickedMenuItemId);
+    // handle menu item click
+    menuItemsContainer.self.addEventListener("click", (e) => {
+      if (e.target.closest(".menu__item")) {
+        console.log("menu item was clicked");
+        const clickedMenuItem = e.target.closest(".menu__item");
+        const clickedMenuItemId = clickedMenuItem.getAttribute("data-id");
+        console.log(clickedMenuItemId);
 
-      // get modal data by id
-      const data = modalService.getDataItemById(clickedMenuItemId);
+        // get modal data by id
+        const data = modalService.getDataItemById(clickedMenuItemId);
 
-      // render modal
-      const modal = new MenuItemModal(data);
-      modal.renderModal();
-    }
-  });
+        // render modal
+        const modal = new MenuItemModal(data);
+        modal.renderModal();
+      }
+    });
+  }
 };
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,111 @@
 export class Slider {
-    constructor() {
-        
+  constructor() {
+    this.arrowLeft = document.querySelector(".slider__arrow_left");
+    this.arrowRight = document.querySelector(".slider__arrow_right");
+    this.carousel = document.querySelector(".slider__slides");
+    this.slides = document.querySelectorAll(".slide");
+    this.controlsContainer = document.querySelector(".controls");
+    this.controls = document.querySelectorAll(".control");
+  }
+
+  left = (e) => {
+    if (e.target.closest(".slider__arrow_left")) {
+      const activeSlideIndex = this.getActiveSlideIndex();
+      this.unmarkAllSlides();
+      this.moveSlideRight(activeSlideIndex);
     }
+  };
+
+  right = (e) => {
+    if (e.target.closest(".slider__arrow_right")) {
+      let activeSlideIndex = this.getActiveSlideIndex();
+      this.unmarkAllSlides();
+      this.moveSlideLeft(activeSlideIndex);
+    }
+  };
+
+  bindEvents() {
+    this.arrowLeft.addEventListener("click", this.left);
+    this.arrowRight.addEventListener("click", this.right);
+  }
+
+  getActiveSlideIndex() {
+    const slides = Array.from(this.slides);
+    return slides.findIndex((slide) =>
+      slide.classList.contains("slide_active")
+    );
+  }
+
+  unmarkAllSlides() {
+    const slides = document.querySelectorAll(".slide");
+    slides.forEach((slide) => {
+      if (slide.classList.contains("slide_active")) {
+        slide.classList.remove("slide_active");
+      }
+      if (slide.classList.contains("slide_prev")) {
+        slide.classList.remove("slide_prev");
+      }
+      if (slide.classList.contains("slide_next")) {
+        slide.classList.remove("slide_next");
+      }
+    });
+  }
+
+  moveSlideLeft(activeSlideIndex) {
+    // if current active slide is less than the total number of slides
+    // previousSlidIndex = activeSlideIndex;
+    // activeSlideIndex = activeSlideIndex++
+
+    // else
+    // mark [0] slide as active
+    // previousSlidIndex = activeSlideIndex;
+    // activeSlideIndex = 0;
+    if (activeSlideIndex < this.slides.length - 1) {
+      const prevSlideIndex = activeSlideIndex;
+      activeSlideIndex = ++activeSlideIndex;
+      let percents = activeSlideIndex === 0 ? 100 : activeSlideIndex * 100;
+      this.slides[activeSlideIndex].classList.add("slide_active");
+
+      this.slides[prevSlideIndex].style.transform = `translateX(-${percents}%)`;
+      this.slides[
+        activeSlideIndex
+      ].style.transform = `translateX(-${percents}%)`;
+    } else {
+      activeSlideIndex = 0;
+      let percents = activeSlideIndex;
+      this.slides[activeSlideIndex].classList.add("slide_active");
+
+      this.slides.forEach((slide) => {
+        slide.style.transform = `translateX(${percents}%)`;
+      });
+    }
+  }
+
+  moveSlideRight(activeSlideIndex) {
+    // if current active slide === 0
+    // previousSlidIndex = activeSlideIndex;
+    // activeSlideIndex = slides.length - 1
+
+    // else
+    // previousSlidIndex = activeSlideIndex;
+    // activeSlideIndex = activeSlideIndex--;
+
+    if (activeSlideIndex === 0) {
+      activeSlideIndex = this.slides.length - 1;
+      let percents = activeSlideIndex * 100;
+      this.slides[activeSlideIndex].classList.add("slide_active");
+      this.slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${percents}%`;
+      });
+    } else {
+      const prevSlideIndex = activeSlideIndex;
+      activeSlideIndex = --activeSlideIndex;
+      let percents = activeSlideIndex * 100;
+      this.slides[activeSlideIndex].classList.add("slide_active");
+      this.slides[prevSlideIndex].style.transform = `translateX(-${percents}%)`;
+      this.slides[
+        activeSlideIndex
+      ].style.transform = `translateX(-${percents}%)`;
+    }
+  }
 }
